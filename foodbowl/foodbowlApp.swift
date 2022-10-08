5cc145696d1791e269dd7593e0012cae1f425b03
Spencer Hartland <shartland@apple.com>
2022-10-07 22:16:25 -0700

    Initial Commit

---
+++ foodbowl/foodbowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodbowlApp.swift
+//  foodbowl
+//
+//  Created by Spencer Hartland on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct foodbowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
